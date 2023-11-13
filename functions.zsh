3f4fdfa43fdd9e20d9b679d5a3e5e96217cfe918
Jakub Mikołajczyk <kmikolaj@gmail.com>
2023-11-13 21:59:40 +0100

    add tar.zst to unpack

--- functions.zsh
+++ functions.zsh
@@ -44,19 +44,20 @@ vcspack() {
 unpack() {
 	if [ -f $1 ]; then
 		case $1 in
-			*.tar.bz2)       tar xvjf $1     ;;
-			*.tar.gz)        tar xvzf $1     ;;
-			*.tar.xz)        tar xvJf $1     ;;
-			*.bz2)           bunzip2 $1      ;;
-			*.[rR][aA][rR])  unrar x $1      ;;
-			*.gz)            gunzip $1       ;;
-			*.tar)           tar xvf $1      ;;
-			*.tbz2)          tar xvjf $1     ;;
-			*.tgz)           tar xvzf $1     ;;
-			*.[zZ][iI][pP])  unzip $1        ;;
-			*.Z)             uncompress $1   ;;
-			*.7z)            7z x $1         ;;
-			*.xz)            xz -d $1         ;;
+			*.tar.bz2)       tar xvjf $1         ;;
+			*.tar.gz)        tar xvzf $1         ;;
+			*.tar.xz)        tar xvJf $1         ;;
+			*.tar.zst)       tar -I zstd -xvf $1 ;;
+			*.bz2)           bunzip2 $1          ;;
+			*.[rR][aA][rR])  unrar x $1          ;;
+			*.gz)            gunzip $1           ;;
+			*.tar)           tar xvf $1          ;;
+			*.tbz2)          tar xvjf $1         ;;
+			*.tgz)           tar xvzf $1         ;;
+			*.[zZ][iI][pP])  unzip $1            ;;
+			*.Z)             uncompress $1       ;;
+			*.7z)            7z x $1             ;;
+			*.xz)            xz -d $1            ;;
 			*)               echo "File '$1' can't be unpacked." ;;
 		esac
 	else
