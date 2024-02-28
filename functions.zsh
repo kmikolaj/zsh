6575c5a27489a70767a8b208f682e57a572645c3
Jakub Mikołajczyk <kmikolaj@gmail.com>
2024-02-28 18:06:17 +0100

    use ouch to unpack archives if exists

--- functions.zsh
+++ functions.zsh
@@ -42,29 +42,33 @@ vcspack() {
 }
 
 # Ultimate archive unpacker
-unpack() {
-	if [ -f $1 ]; then
-		case $1 in
-			*.tar.bz2)       tar xvjf $1         ;;
-			*.tar.gz)        tar xvzf $1         ;;
-			*.tar.xz)        tar xvJf $1         ;;
-			*.tar.zst)       tar -I zstd -xvf $1 ;;
-			*.bz2)           bunzip2 $1          ;;
-			*.[rR][aA][rR])  unrar x $1          ;;
-			*.gz)            gunzip $1           ;;
-			*.tar)           tar xvf $1          ;;
-			*.tbz2)          tar xvjf $1         ;;
-			*.tgz)           tar xvzf $1         ;;
-			*.[zZ][iI][pP])  unzip $1            ;;
-			*.Z)             uncompress $1       ;;
-			*.7z)            7z x $1             ;;
-			*.xz)            xz -d $1            ;;
-			*)               echo "File '$1' can't be unpacked." ;;
-		esac
-	else
-		echo "'$1' is not an archive format."
-	fi
-}
+if which ouch &> /dev/null; then
+	alias unpack='ouch decompress'
+else
+	unpack() {
+		if [ -f $1 ]; then
+			case $1 in
+				*.tar.bz2)       tar xvjf $1         ;;
+				*.tar.gz)        tar xvzf $1         ;;
+				*.tar.xz)        tar xvJf $1         ;;
+				*.tar.zst)       tar -I zstd -xvf $1 ;;
+				*.bz2)           bunzip2 $1          ;;
+				*.[rR][aA][rR])  unrar x $1          ;;
+				*.gz)            gunzip $1           ;;
+				*.tar)           tar xvf $1          ;;
+				*.tbz2)          tar xvjf $1         ;;
+				*.tgz)           tar xvzf $1         ;;
+				*.[zZ][iI][pP])  unzip $1            ;;
+				*.Z)             uncompress $1       ;;
+				*.7z)            7z x $1             ;;
+				*.xz)            xz -d $1            ;;
+				*)               echo "File '$1' can't be unpacked." ;;
+			esac
+		else
+			echo "'$1' is not an archive format."
+		fi
+	}
+fi
 
 # hexstring to binary stream
 hextostr() {
