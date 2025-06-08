731324bb7090a6fc20f28b970762788721c9d46e
Jakub Mikołajczyk <kmikolaj@gmail.com>
2025-06-08 13:56:24 +0200

    update plugins

--- copybuffer.plugin.zsh
+++ copybuffer.plugin.zsh
@@ -1,4 +1,6 @@
-# Copy command to clipoard
+# copy the active line from the command line buffer
+# onto the system clipboard
+
 copybuffer () {
   if which xcopy &>/dev/null; then
     printf "%s" "$BUFFER" | xcopy
@@ -9,3 +11,4 @@ zle -N copybuffer
 
 bindkey -M emacs "^O" copybuffer
 bindkey -M viins "^O" copybuffer
+bindkey -M vicmd "^O" copybuffer
--- keychain.plugin.zsh
+++ keychain.plugin.zsh
@@ -3,15 +3,14 @@
 # Define SHORT_HOST if not defined (%m = host name up to first .)
 SHORT_HOST=${SHORT_HOST:-${(%):-%m}}
 
-local agents=gpg,ssh
-local args=(--quiet --noask)
+local args=(--quiet --noask --ssh-allow-gpg)
 local -a identities
 
 local _keychain_env_sh
 local _keychain_env_sh_gpg
 
 # start keychain...
-keychain ${args} --agents ${agents} ${identities} --host $SHORT_HOST
+keychain ${args} ${identities} --host $SHORT_HOST
 
 # Get the filenames to store/lookup the environment from
 _keychain_env_sh="$HOME/.keychain/$SHORT_HOST-sh"
